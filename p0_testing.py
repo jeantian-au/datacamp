5798e38ae1cbe6dde9dc2c2a2cc42bdcbd0ffc9e
Jean Tian <jean.tian.tech@gmail.com>
2026-07-07 21:27:11 +1000

    W7 D2: Complete Data Manipulation with pandas (Ch3-4) - certified

--- p0_testing.py
+++ p0_testing.py
@@ -1,29 +1,29 @@
-# scores = [85, 92, 78, 90, 66, 100, 73]
-# print(scores[2])
-# print(scores[-2:])
+scores = [85, 92, 78, 90, 66, 100, 73]
+print(scores[2])
+print(scores[-2:])
 
-# passing = [score for score in scores if score >= 80]
-# print(passing)
-# print(len(passing))
+passing = [score for score in scores if score >= 80]
+print(passing)
+print(len(passing))
 
-# student = {"name": "Amy", "score": 92, "grade": "A"}
+student = {"name": "Amy", "score": 92, "grade": "A"}
 
-# score = student.get("score", 0)
+score = student.get("score", 0)
 
-# print(score)
-# if score >= 80:
-#     student["passed"] = True
-# else:
-#     student["passed"] = False
-# print(student)
+print(score)
+if score >= 80:
+    student["passed"] = True
+else:
+    student["passed"] = False
+print(student)
 
-# students = [
-#     {"name": "Amy", "score": 92},
-#     {"name": "Ben", "score": 65},
-#     {"name": "Cara", "score": 88},
-# ]
-# names = [student["name"] for student in students if student["score"] >= 80]
-# print(names)
+students = [
+    {"name": "Amy", "score": 92},
+    {"name": "Ben", "score": 65},
+    {"name": "Cara", "score": 88},
+]
+names = [student["name"] for student in students if student["score"] >= 80]
+print(names)
 
 
 temps = [15, 22, 18, 30, 12, 27, 19, 33, 8, 25]
